d9bcf4128eae04e5354a02ef28be90873cb4a520
Billy Gray <wgray@zetetic.net>
2016-07-28 14:24:30 -0400

    Adds OS X framework (Zxcvbn-OSX, product name Zxcvbn) and runs original unit tests

---
+++ Zxcvbn-OSX/Zxcvbn-OSX.h
@@ -0,0 +1,19 @@
+//
+//  Zxcvbn-OSX.h
+//  Zxcvbn-OSX
+//
+//  Created by Billy Gray on 7/28/16.
+//  Copyright © 2016 Dropbox. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Zxcvbn-OSX.
+FOUNDATION_EXPORT double Zxcvbn_OSXVersionNumber;
+
+//! Project version string for Zxcvbn-OSX.
+FOUNDATION_EXPORT const unsigned char Zxcvbn_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Zxcvbn_OSX/PublicHeader.h>
+
+
